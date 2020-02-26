6ac85ea13feeeef3b66c4098690ffba9a32fcdc1
tiange.zhao <tiange.zhao@hobot.cc>
2020-02-26 18:02:36 +0800

    feature: sandbox.config

---
+++ tiange_steps.sh
@@ -0,0 +1,4 @@
+# 目前部署的 sandbox url 是：http://10.10.13.126:30081/console
+# 访问之则可看到所有的 projects, domains, workflows, executions 等内容
+
+# workflow 所运行的 python 代码路径在这里指定：workflow_packages 参数 in sandbox.config
