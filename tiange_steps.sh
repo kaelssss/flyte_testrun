65064d2cf845a37a69a91ed89ca276b40678cbbc
tiange.zhao <tiange.zhao@hobot.cc>
2020-02-26 18:02:45 +0800

    feature: sandbox.config

--- tiange_steps.sh
+++ tiange_steps.sh
@@ -1,4 +1,6 @@
 # 目前部署的 sandbox url 是：http://10.10.13.126:30081/console
 # 访问之则可看到所有的 projects, domains, workflows, executions 等内容
 
-# workflow 所运行的 python 代码路径在这里指定：workflow_packages 参数 in sandbox.config
+# sandbox.config 中指定：
+# platform.url: http://10.10.13.126:30081
+# aws？（使用了 Types.Blob 作为输出类型，该类型会去找一个远程仓库来存储这些大文件）
